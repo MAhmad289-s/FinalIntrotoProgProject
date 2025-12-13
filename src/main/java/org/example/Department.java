5b7a8779a5a597aaecffcaeee4159867d8a93135
MAhmad289-s <ninjaahmadsaqib@gmail,com>
2025-12-12 19:05:17 -0500

    Department class created

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -1,5 +1,23 @@
 package org.example;
 
 public class Department {
-    String departmentID;
+    private String departmentId;      // D01, D02, ...
+    private String departmentName;
+    public static int nextId = 1;
+
+
+
+
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null) {
+            return false;
+        }
+        for (int i = 0; i < departmentName.length(); i++) {
+            char c = departmentName.charAt(i);
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
 }
