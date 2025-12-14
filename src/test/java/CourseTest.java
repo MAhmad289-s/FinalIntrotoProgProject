7723688c99a64aeb8ca506a90a48af8a6049032b
MAhmad289-s <ninjaahmadsaqib@gmail,com>
2025-12-13 19:02:36 -0500

    Deleted AssignmentTest.java and fixed error wiTH Addassigment

--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -8,6 +8,18 @@ import org.junit.jupiter.api.Test;
 
 public class CourseTest {
     @Test
+    @DisplayName("Assignment weights sum to 100 -> true")
+    void isAssignmentWeightValid1() {
+        Department department = new Department("Math");
+        Course course = new Course("Calculus", 3.0, department);
+        course.AddAssignment("Test1", 40, 100);
+        course.AddAssignment("Test2", 40, 100);
+        boolean actual = course.isAssignmentWeightValid();
+        boolean expected = false;
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
     @DisplayName("Register new student -> true")
     void registerStudent1() {
         Department department = new Department("Math");
@@ -19,7 +31,7 @@ public class CourseTest {
         Assertions.assertEquals(expected, actual);
     }
     @Test
-    @DisplayName("Single student, single assignment -> average")
+    @DisplayName(" student,  assignment -> average")
     void calcStudentsAverage1() {
         Department d = new Department("Math");
         Course c = new Course(" Linear Algebra", 3.0, d);
@@ -33,11 +45,11 @@ public class CourseTest {
         Assertions.assertArrayEquals(expected, actual);
     }
     @Test
-    @DisplayName("Add assignment with no students -> scores list empty")
+    @DisplayName("Add assignment with no students -> 0")
     void addAssignment1() {
         Department department = new Department("CS");
         Course course = new Course("Programming", 3.0, department);
-        course.AddAssignment("Assignment1", 100, 100);
+        course.AddAssignment("Assignment1", 25, 100);
         int actual = course.getAssignments().get(0).getScores().size();
         int expected = 0;
         Assertions.assertEquals(expected, actual);
